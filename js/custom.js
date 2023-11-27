44d88e6c52b24c93272e0f23a2348139a72f5f92
doriana_lab9 <152193307+meccworkshop@users.noreply.github.com>
2023-11-27 16:01:40 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // HERO SLIDE
+    $('.hero-slide').backstretch([
+      "images/slideshow/athens2.jpg",
+      "images/slideshow/syntagma2.jpg",
+      "images/slideshow/agora.jpg",
+    ],  {duration: 2000, fade: 750});
+
+    // REVIEWS CAROUSEL
+    $('.reviews-carousel').owlCarousel({
+    items:3,
+    loop:true,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    margin:30,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:2
+        },
+        1000:{
+          items:3
+        }
+      }
+    })
+
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+    }
+});
+    
+  })(window.jQuery);
+
+
